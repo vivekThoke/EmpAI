932e3fb5f2244f66f12d86f376edb04960aecdae
vivekThoke <vivekthoke5@gmail.com>
2026-04-19 20:37:05 +0530

    generate jwt token service

--- EmployeePortal.Infrastructure/Services/TokenService.cs
+++ EmployeePortal.Infrastructure/Services/TokenService.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using EmployeePortal.Application;
 using EmployeePortal.Application.Interfaces;
+using EmployeePortal.Domain.Entities;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 
 namespace EmployeePortal.Infrastructure.Services
@@ -17,6 +22,33 @@ namespace EmployeePortal.Infrastructure.Services
         public TokenService(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, user.Email),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+            );
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+             var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(
+                    Convert.ToDouble(_configuration["Jwt:ExpiryMinutes"])
+                ),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }

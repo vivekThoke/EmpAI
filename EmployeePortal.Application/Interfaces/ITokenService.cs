e8e87d05e612b431ca3654272f79bd6f4eaece80
vivekThoke <vivekthoke5@gmail.com>
2026-04-19 13:34:46 +0530

    interfaces for generate token

---
+++ EmployeePortal.Application/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeePortal.Domain.Entities;
+
+namespace EmployeePortal.Application.Interfaces
+{
+    internal interface ITokenService
+    {
+        string GenerateToken(User user);
+    }
+}

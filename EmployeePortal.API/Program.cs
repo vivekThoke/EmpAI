d03d5a9ed2490ab0fab7741a96475811ca585343
vivekThoke <vivekthoke5@gmail.com>
2026-04-19 12:26:15 +0530

    done with database migration and updated the database

--- EmployeePortal.API/Program.cs
+++ EmployeePortal.API/Program.cs
@@ -1,6 +1,15 @@
+using EmployeePortal.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Add DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

b12e16c1cfbade462093e7edae84f54c10f3f8c3
vivekThoke <vivekthoke5@gmail.com>
2026-04-19 13:19:20 +0530

    auth controller for register

---
+++ EmployeePortal.API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using EmployeePortal.Application.DTOs;
+using EmployeePortal.Domain.Entities;
+using EmployeePortal.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterDto dto)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                Role = "Employee"
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok("User registered successfully");
+        }
+    }
+}

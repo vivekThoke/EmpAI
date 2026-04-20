67ab78c41f90469a64edb8e6e1748a4131220cdc
vivekThoke <vivekthoke5@gmail.com>
2026-04-20 20:35:19 +0530

    register user async for auth service

--- EmployeePortal.Infrastructure/Services/AuthService.cs
+++ EmployeePortal.Infrastructure/Services/AuthService.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EmployeePortal.Application.DTOs;
 using EmployeePortal.Application.Interfaces;
+using EmployeePortal.Domain.Entities;
 using EmployeePortal.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace EmployeePortal.Infrastructure.Services
 {
@@ -17,6 +21,28 @@ namespace EmployeePortal.Infrastructure.Services
         {
             _dbContext = dbContext;
             _tokenService = tokenService;
+        }
+
+        public async Task<string> RegisterAsync(RegisterDto dto)
+        {
+            var emailExists = await _dbContext.Users.AnyAsync(x => x.Email == dto.Email);
+
+            if (emailExists)
+                throw new Exception("Email already exists");
+
+            var user = new User
+            {
+                Id = Guid.NewGuid(),
+                Name = dto.Name,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                Role = "Employee"
+            };
+
+            _dbContext.Users.Add(user);
+            await _dbContext.SaveChangesAsync();
+
+            return "User Register Successfully";
         }
     }
 }

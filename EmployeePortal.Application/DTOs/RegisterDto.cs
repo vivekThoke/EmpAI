d5bb04d3a5970f2c088212ca845f27ab5dbc641e
vivekThoke <vivekthoke5@gmail.com>
2026-04-20 22:46:09 +0530

    validation for the register dto

--- EmployeePortal.Application/DTOs/RegisterDto.cs
+++ EmployeePortal.Application/DTOs/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,15 @@ namespace EmployeePortal.Application.DTOs
 {
     public class RegisterDto
     {
+        [Required]
         public string ?Name { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string ?Email { get; set; }
+
+        [Required]
+        [MinLength(6)]
         public string ?Password { get; set; }
     }
 }

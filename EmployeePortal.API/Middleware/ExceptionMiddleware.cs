da8959f9589fe3fb06892a77e6b208c51a1e82d6
vivekThoke <vivekthoke5@gmail.com>
2026-04-20 23:00:15 +0530

    global exception handler

---
+++ EmployeePortal.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace EmployeePortal.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                context.Response.ContentType = "application/json";
+
+                var response = new { error = ex.Message };
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+        }
+    }
+}
--- EmployeePortal.Application/DTOs/LoginDto.cs
+++ EmployeePortal.Application/DTOs/LoginDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace EmployeePortal.Application.DTOs
 {
     public class LoginDto
     {
+        [Required]
         public string ?Email { get; set; }
+
+        [Required]
         public string ?Password { get; set; }
     }
 }

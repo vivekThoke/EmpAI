41f4d11b2297a41bef1db8d7ec6d19c44466c095
vivekThoke <vivekthoke5@gmail.com>
2026-04-23 21:37:39 +0530

    polished the login page

--- employee-portal-ui/app/login/page.tsx
+++ employee-portal-ui/app/login/page.tsx
@@ -5,7 +5,7 @@ import api from "../services/apiClient";
 import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: {errors}, } = useForm();
     const router = useRouter();
 
     const onSubmit = async (data: any) => {
@@ -15,17 +15,29 @@ export default function LoginPage() {
             router.push("/dashboard");
         }
         catch (err: any) {
-            console.log(err.response?.data);
+            alert(err.response?.data || "Login Failed");
         }
     };
 
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("email")} placeholder="Email" />
-                <input {...register("password")} type="password" placeholder="Password" />
-                <button type="submit">Login</button>
+        <div className="flex items-center justify-center h-screen">
+            <form onSubmit={handleSubmit(onSubmit)}
+                    className="bg-white p-6 rounded shadow-md w-80">
+                <h2 className="text-xl mb-4">Login</h2>
+
+                <input 
+                    {...register("email", {required: "Email is required"})} 
+                    placeholder="Email"
+                    className="w-full mb-3 p-2 border rounded" />
+
+                <input 
+                    {...register("password", {required: "Password is required"})} 
+                    type="password" 
+                    placeholder="Password"
+                    className="w-full mb-3 p-2 border rounded" />
+
+                <button type="submit"
+                        className="w-full bg-blue-500 text-white p-2 rounded">Login</button>
             </form>
         </div>
     )

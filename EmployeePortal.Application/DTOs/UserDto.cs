3075464fd1819f1d0a1269f7754cc430fb25bf10
vivekThoke <vivekthoke5@gmail.com>
2026-04-27 09:21:14 +0530

    User DTO

---
+++ EmployeePortal.Application/DTOs/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePortal.Application.DTOs
+{
+    public class UserDto
+    {
+        public string ?Name { get; set; }
+        public string ?Email { get; set; }
+        public string ?Department { get; set; }
+        public string ?Designation { get; set; }
+    }
+}

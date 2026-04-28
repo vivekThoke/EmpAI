659c7974a9b0ed804db2c6b496ff41361fc7914c
vivekThoke <vivekthoke5@gmail.com>
2026-04-28 23:15:40 +0530

    implmentation of user service

---
+++ EmployeePortal.Infrastructure/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeePortal.Application.Interfaces;
+using EmployeePortal.Infrastructure.Data;
+
+namespace EmployeePortal.Infrastructure.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly AppDbContext _context;
+
+        public UserService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}

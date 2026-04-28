23978d34e49024f179f2b9e11f8f753b840179ef
vivekThoke <vivekthoke5@gmail.com>
2026-04-28 23:12:29 +0530

    interface for the User service

---
+++ EmployeePortal.Application/Interfaces/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeePortal.Application.DTOs;
+
+namespace EmployeePortal.Application.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserDto> GetCurrentUserAsync(Guid userId);
+        Task<List<UserDto>> GetAllUserAsync();
+        Task UpdateUserAsync(Guid userId, UpdateUserDto dto);
+    }
+}

50ff9fd9c3bbe701749debaf13fdce03ca99670a
vivekThoke <vivekthoke5@gmail.com>
2026-04-29 22:49:17 +0530

    dependency for the User sevice

--- EmployeePortal.API/Program.cs
+++ EmployeePortal.API/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IUserService, UserService>();
 
 
 

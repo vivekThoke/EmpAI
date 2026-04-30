39dc61a2750748bb079cc8f4f10cc4c51ef6980f
vivekThoke <vivekthoke5@gmail.com>
2026-04-30 22:02:33 +0530

    route for get all users for admin

--- EmployeePortal.API/Controllers/UserController.cs
+++ EmployeePortal.API/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Security.Claims;
+using EmployeePortal.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeePortal.API.Controllers
@@ -7,11 +9,31 @@ namespace EmployeePortal.API.Controllers
     [Route("api/[controller]")]
     public class UserController : Controller
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        private Guid GetUserId()
+        {
+            return Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+        }
+
         [Authorize]
         [HttpGet("me")]
         public IActionResult GetMe()
         {
             return Ok("You are authenticated");
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAllUserAsync();
+            return Ok(users);
         }
     }
 }

fb5bad003ef355347d33f51f747d915061dac2a2
vivekThoke <vivekthoke5@gmail.com>
2026-05-02 20:48:42 +0530

    updated the other half of the profile page

--- employee-portal-ui/app/profile/page.tsx
+++ employee-portal-ui/app/profile/page.tsx
@@ -27,4 +27,45 @@ export default function ProfilePage() {
 
         fetchUser();
     }, []);
+
+    const handleUpdate = async (e: any) => {
+        e.preventDefault();
+
+        await api.put("/user", form);
+
+        alert("Profile updated successfull");
+
+        router.push("/dashboard");
+    };
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl mb-4">Update Profile</h1>
+
+            <form onSubmit={handleUpdate} className="space-y-4">
+                <input className="border p-2 w-full"
+                        placeholder="Name"
+                        value={form.name}
+                        onChange={(e) => setForm({ ...form, name: e.target.value})} 
+                />
+
+                <input className="border p-2 w-full"
+                        placeholder="Department"
+                        value={form.department}
+                        onChange={(e) => setForm({ ...form, department: e.target.value})}
+                />
+
+                <input className="border p-2 w-full"
+                        placeholder="Designation"
+                        value={form.designation}
+                        onChange={(e) => setForm({ ...form, designation: e.target.value})}
+                />
+
+                <button className="bg-blue-500 text-white px-4 py-2 rounded">
+                    Save
+                </button>
+            </form>
+        </div>
+    )
+
 }

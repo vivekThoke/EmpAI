5100ebc74819b5d0438465a0fb8fb5cb068e713e
vivekThoke <vivekthoke5@gmail.com>
2026-05-03 19:37:10 +0530

    fetch users for the employee page

--- employee-portal-ui/app/dashboard/employees/page.tsx
+++ employee-portal-ui/app/dashboard/employees/page.tsx
@@ -1,5 +1,7 @@
 "use-client";
 
+import api from "@/app/services/apiClient";
+import { getUserRole } from "@/app/services/jwt";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -9,6 +11,26 @@ export default function EmployeePage() {
     const router = useRouter();
 
     useEffect(() => {
+        const role = getUserRole();
 
-    });
+        if (role !== "Admin") {
+            router.push("/dashboard");
+            return;
+        }
+
+        const fetchUsers = async () => {
+            try {
+                const res = await api.get("/user");
+
+                setUsers(res.data);
+            } catch {
+                alert("Failed to load employees");
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+
+    
 }   
